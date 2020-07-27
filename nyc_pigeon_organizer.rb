2430a9ece74770d7f2208c5981dbc99ae9229d1d
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-27 03:38:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  the_hash = {}
+
+  data.each do |attribute, hash|
+    hash.each do |property, names|
+      names.each do |name|
+        if !the_hash.keys.include?(name)
+          the_hash[name] = {}
+        end
+        if !the_hash[name].keys.include?(attribute)
+            the_hash[name][attribute] = []
+        end
+        the_hash[name][attribute] << property.to_s
+      end
+    end
+  end
+
+  the_hash
 end
